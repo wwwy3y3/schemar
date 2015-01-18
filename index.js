454a4b595d6cb03a886ccc7477ddcff266baa024
william <wwwy3y3@gmail.com>
2015-01-19 02:17:13 +0800

    done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _= require('lodash');
 /*
 parse
 parse obj to schema
@@ -5,11 +6,69 @@ ex: { "name": "Alan", "hometown": "Somewhere, TX",
              "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]}
     ->
     {
-		"name": String,
-		"hometown": String,
-		"kids": [{ "name": String, "age": Number }]
+		"name": "string",
+		"hometown": "string",
+		"kids": [{ "name": "string", "age": "number" }]
     }
 */
 exports.parse= function (obj) {
 	//obj -> schema
+	return chkType(obj);
+}
+
+function chkType (val) {
+	// if null
+	if(_.isNull(val))
+		return 'null';
+
+	// if undefined
+	if(_.isUndefined(val))
+		return 'undefined';
+
+	// if string
+	if(_.isString(val))
+		return 'string';
+
+	if(_.isNumber(val))
+		return 'number';
+
+	if(_.isBoolean(val))
+		return 'boolean';
+
+	if(_.isDate(val))
+		return 'date';
+
+	// if array
+	if(_.isArray(val)){
+		// recursive
+		var ret= [];
+		val.forEach(function (element, index) {
+			var obj= {};
+				obj[index]= chkType(element);
+			ret.push(obj);
+		})
+		return ret;
+	}
+
+	// if object
+	// go nested
+	if(_.isPlainObject(val)){
+		var ret= {};
+		for(key in val){
+			var obj= {};
+				obj[key]= chkType(val[key]);
+		}
+		return ret;
+	}
+
+	// if it's a function
+	if(_.isFunction(val))
+		return 'function';
+
+	// if unknown object
+	if(_.isObject(val))
+		return val.constructor.name; // return constructor name
+
+	//default return 
+	return 'object';
 }
