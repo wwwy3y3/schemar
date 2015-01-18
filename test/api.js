5eecf6a4bb23d1cc32db1a82ef12fb4541dedaa3
william <wwwy3y3@gmail.com>
2015-01-19 02:21:24 +0800

    add test

---
+++ test/api.js
@@ -0,0 +1,4 @@
+var schemar= require('../');
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+console.log(schemar.parse(data));
