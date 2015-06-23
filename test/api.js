25ab4f061d7531ab339b9fb6c6edf5cc74e09a40
william <wwwy3y3@gmail.com>
2015-06-23 22:32:08 +0800

    add cans tests

--- test/api.js
+++ test/api.js
@@ -1,47 +1,13 @@
 var schemar= require('../');
 var Datas= require('./datas');
+var Layouts= require('./layouts');
 var jsome= require('jsome');
 
 jsome(schemar.parse(Datas.varies, { textThres: 20 }));
 jsome(schemar.parse(Datas.nested, { textThres: 20 }));
-
-var obj2= schemar.jsonSchema(Datas.jeremy, 
-	{ 
-		textThres: 20,
-		schema: {
-			favorite_color: {
-				title: 'favorite color'
-			},
-
-			gender: {
-				enum: [ "male", "female" ]
-			},
-
-			location: {
-				properties:{
-					citystate: {
-						description: "This is generated automatically from the previous two fields",
-						template: "{{city}}, {{state}}",
-						watch: {
-							city: "location.city",
-							state: "location.state"
-						}
-					}
-				}
-			},
-
-			pets: {
-				title: 'Pets',
-				properties: {
-					type:{
-						enum: [ "cat", "dog", "bird", "reptile", "other" ],
-			            default: "dog"
-					}
-				},
-				uniqueItems: true,
-				format: 'table'
-			}
-		}
- 	}, 'sparta!!');
-console.log(JSON.stringify(schemar.jsonSchema(Datas.joe), null, 4));
+jsome(schemar.jsonSchema(Datas.jeremy, Layouts.jeremy, 'sparta!!'));
+jsome(schemar.jsonSchema(Datas.joe));
+jsome(schemar.jsonSchema(Datas.resume));
+jsome(schemar.jsonSchema(Datas.strata));
+//console.log(JSON.stringify(schemar.jsonSchema(Datas.strata), null, 4));
 
