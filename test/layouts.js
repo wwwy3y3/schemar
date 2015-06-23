6fad98c6aa0e0f33d040a6b37d456fd3507afcb6
william <wwwy3y3@gmail.com>
2015-06-23 22:31:59 +0800

    layouts

---
+++ test/layouts.js
@@ -0,0 +1,37 @@
+exports.jeremy= { 
+	textThres: 20,
+	schema: {
+		favorite_color: {
+			title: 'favorite color'
+		},
+
+		gender: {
+			enum: [ "male", "female" ]
+		},
+
+		location: {
+			properties:{
+				citystate: {
+					description: "This is generated automatically from the previous two fields",
+					template: "{{city}}, {{state}}",
+					watch: {
+						city: "location.city",
+						state: "location.state"
+					}
+				}
+			}
+		},
+
+		pets: {
+			title: 'Pets',
+			properties: {
+				type:{
+					enum: [ "cat", "dog", "bird", "reptile", "other" ],
+		            default: "dog"
+				}
+			},
+			uniqueItems: true,
+			format: 'table'
+		}
+	}
+}
